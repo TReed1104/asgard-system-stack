78acf580d4f05637f88b5615b1e8a38a331cbd2e
Tom Reed <t.reed1104@gmail.com>
2019-09-23 11:59:14 +0100

    Added a version of the deployment script, that doesn't run the build step if an image is present

---
+++ run_asgard_no_build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo ---------------------------------------
+echo Creating Local MySQL storage volume
+echo ---------------------------------------
+mkdir volumes
+mkdir volumes/db
+echo
+echo ---------------------------------------
+echo Building and Deploying Asgard
+echo ---------------------------------------
+docker-compose -f docker-compose.yml up -d --remove-orphans
+echo
+echo ---------------------------------------
+echo Conntainer Status:
+echo ---------------------------------------
+docker ps | grep 'asgard\|heimdall\|mimir'
