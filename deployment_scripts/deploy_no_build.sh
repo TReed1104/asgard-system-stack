b1f111240d330a70b93d7f5229682716a29bf29b
Tom Reed <t.reed1104@gmail.com>
2020-01-02 15:55:50 +0000

    Added Odin to deployment script grep for our container status

--- deployment_scripts/deploy_no_build.sh
+++ deployment_scripts/deploy_no_build.sh
@@ -15,4 +15,4 @@ echo
 echo ---------------------------------------
 echo Conntainer Status:
 echo ---------------------------------------
-docker ps | grep 'asgard\|heimdall\|mimir\|yggdrasil'
+docker ps | grep 'asgard\|heimdall\|mimir\|yggdrasil\|odin'
