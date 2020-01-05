672a213bca70b5cd587bb70fa13ca758e28de68e
Tom Reed <t.reed1104@gmail.com>
2020-01-05 22:33:25 +0000

    Added line breaks to make the links a bit easier to use

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -2,8 +2,11 @@
     <div class="container">
         <h1 class="page-title">Asgard</h1>
         <p>Welcome to the work in progress landing page for Asgard.</p>
+        <br/>
         <a href="/heimdall">Heimdall</a>
+        <br/>
         <a href="/mimir">Mimir</a>
+        <br/>
         <a href="/yggdrasil">Yggdrasil</a>
     </div>
 </template>
