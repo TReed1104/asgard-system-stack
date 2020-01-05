699a2b2fc1c150c78aaa6c445d7bdc44fbc434ff
Tom Reed <t.reed1104@gmail.com>
2020-01-05 22:32:43 +0000

    Added links for the three frontends to the Asgard landing page

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -2,6 +2,9 @@
     <div class="container">
         <h1 class="page-title">Asgard</h1>
         <p>Welcome to the work in progress landing page for Asgard.</p>
+        <a href="/heimdall">Heimdall</a>
+        <a href="/mimir">Mimir</a>
+        <a href="/yggdrasil">Yggdrasil</a>
     </div>
 </template>
 
